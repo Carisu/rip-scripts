6fff1b68e8242b6789f79802a1e9cc81753f4182
Carisu <chris.e.j.davis@gmail.com>
2020-10-22 15:40:56 +0100

    Added extra codecs and changes p4 to AAC

--- ripdvd.ps1
+++ ripdvd.ps1
@@ -2,11 +2,42 @@ param (
 	[Parameter(Position=0)][String]$InputFile,
 	[String]$DvdDrive = "e",
 	[String]$VlcPath = "C:\Program Files\VideoLAN\VLC\vlc.exe",
-	[Parameter(ValueFromPipeline=$true)]$data
+	[Parameter(ValueFromPipeline=$true)]$data,
+	[String]$Encode = "mp4",
+	[String]$AudioEncode,
+	[String]$VideoEncode
 )
 if ($InputFile -and !$data)
 {
 	$data = Get-Content $InputFile
+}
+$Encodings = @{
+	mp4 = @{
+		Audio = "aac";
+		Video = "h264";
+		Mux = "mp4";
+		Extension = "mp4"
+	};
+	mpg = @{
+		Audio = "mpg2a";
+		Video = "mpg2v";
+		Mux = "ps";
+		Extension = "mpg"
+	}
+}
+$Encoding = $Encodings.$Encode
+if (!$AudioEncode) {
+	$AudioEncode = $Encoding.Audio
+}
+if (!$VideoEncode) {
+	$VideoEncode = $Encoding.Video
+}
+$EncodeStrings = @{
+	h264 = "vcodec=h264,fps=25,vfilter=deinterlace";
+	aac = "acodec=mp4a,ab=128,samplerate=48000";
+	mpg2v = "vcodec=mpgv,fps=25,vfilter=deinterlace";
+	mp3 = "acodec=mp3,ab=128,samplerate=48000";
+	mpg2a = "acodec=mpga,ab=192,samplerate=48000"
 }
 $Dvd = $data[0] | ConvertFrom-CSV -Delimiter `t -Header "Type","Title","Creator"
 if ($Dvd.Type = "music")
@@ -51,9 +82,9 @@ $CommandArgs = @("--sout-deinterlace-mode=yadif2x")
 	$CommandArgs += "dvdsimple:///" + $DvdDrive + ":/#" + $TitleChapter
 	$DestPos = $Pad + $Pos
 	$DestPos = $DestPos.substring($DestPos.Length - $Pad.Length - 1)
-	$DestInputFile = $Path + "\" + $DestPos + " " + $Artist + " - " + $Title + ".mp4"
+	$DestInputFile = $Path + "\" + $DestPos + " " + $Artist + " - " + $Title + "." + $Encoding.Extension
 	$DestInputFile = "'" + ((($DestInputFile  -replace "\\", "/") -replace "'", "\'") -replace "`"", "\'\'") + "'"
-	$CommandArgs += ":sout=#transcode{vcodec=h264,fps=25,,vfilter=deinterlace,acodec=mp3,ab=128,samplerate=48000}:std{access=file,mux=ts,dst=" + $DestInputFile + "}"
+	$CommandArgs += ":sout=#transcode{" + $EncodeStrings.$VideoEncode + "," + $EncodeStrings.$AudioEncode + "}:std{access=file,mux=" + $Encoding.Mux + ",dst=" + $DestInputFile + "}"
 }
 $CommandArgs += "vlc://quit"
 & $VlcPath $CommandArgs
